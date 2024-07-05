4c4fbafcc4629838063e5730c4a2187a57d22f42
Victor Gasperi <victorgasperi@gmail.com>
2024-07-05 13:47:44 -0300

    Add screen

---
+++ src/Screens/MainScreen.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class MainScreen extends JFrame{
+    
+    public MainScreen(){
+        super("Gerenciador Financeiro");
+
+        initializeScreen();
+    }
+
+    private void initializeScreen() {
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // pack();
+        setVisible(true);
+    }
+
+}
